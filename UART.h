a30369b50b1dbefb43d0f318497fce0af4736055
MCLEANS <jackagwaya@gmail.com>
2020-05-30 17:49:48 +0300

    declare UART class methods and variables

--- UART.h
+++ UART.h
@@ -1,13 +1,37 @@
 #ifndef _UART_H
 #define _UART_H 
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define BUFFER_SIZE 256
+
+char receive_buffer[BUFFER_SIZE];
+
+
 
 class UART{
 
     private:
+        
+        
     private:
+        void set_baudrate();
+        void flush_buffer();
+        void send_char(char data);
+        char receive_char() const;
+        
+
     public:
+        uint8_t buffer_position = 0;
+        bool endline = false;
+
     public:
+        void init_UART(uint32_t baudrate);
+        void send_string(char *data);
+        void listen();
+
 
 };
 
