0865e3f2540841d3f5a02cc9bfc2307f3d3bf8ae
MCLEANS <jackagwaya@gmail.com>
2020-05-31 10:05:14 +0300

    added Declaration of ADC class'

---
+++ ADC.h
@@ -0,0 +1,21 @@
+#ifndef _ADC_H
+#define _ADC_H
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+class ADC{
+    private:
+    private:
+    public:
+        uint16_t value;
+    public:
+        ADC();
+        void init();
+        uint16_t get_value();
+};
+
+
+
+#endif //_ADC_H
