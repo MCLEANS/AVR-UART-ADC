c5027cee41bd13b8ac708fe4dfca8beeda672d6c
MCLEANS <jackagwaya@gmail.com>
2020-05-31 10:06:59 +0300

    Added ADC initialization

---
+++ ADC.cpp
@@ -0,0 +1,21 @@
+#include "ADC.h"
+
+ADC::ADC(){
+
+}
+
+void ADC::init(){
+    //set ADC prescaler between 5 and 20 for 1MHz clock
+	//Here set to 16
+	ADCSRA |= (1<<ADPS2);
+	//Enable ADC interrupt
+	ADCSRA |= (1<<ADIE);
+	//set ADC reference voltage
+	ADMUX |= (1<<REFS0);
+	//set ADC data to left adjusted
+	ADMUX |= (1<<ADLAR);
+	//Enable ADC
+	ADCSRA |= (1<<ADEN);
+	//start first ADC conversion
+	ADCSRA |= (1<<ADSC);
+}
