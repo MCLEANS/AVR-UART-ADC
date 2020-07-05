00ed17189e1dfa6cc093a395eec64838190fd177
MCLEANS <jackagwaya@gmail.com>
2020-07-05 13:56:22 +0300

    Added listening of extra ADC channels

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ void listen(){
 			uart.send_char(new_line);
 			uart.flush_buffer();
 		}
-		if(strncmp(uart.receive_buffer,FETCH_ADC1,(sizeof(FETCH_ADC1)/sizeof(char))-1) == 0){
+		else if(strncmp(uart.receive_buffer,FETCH_ADC1,(sizeof(FETCH_ADC1)/sizeof(char))-1) == 0){
 			adc.convert();
 			_delay_us(500);
 			itoa(adc_values.channel_1,response,10);
@@ -48,7 +48,7 @@ void listen(){
 			uart.flush_buffer();
 		
 		}
-		if(strncmp(uart.receive_buffer,FETCH_ADC2,(sizeof(FETCH_ADC2)/sizeof(char))-1) == 0){
+		else if(strncmp(uart.receive_buffer,FETCH_ADC2,(sizeof(FETCH_ADC2)/sizeof(char))-1) == 0){
 			adc.convert();
 			_delay_us(500);
 			itoa(adc_values.channel_2,response,10);
@@ -57,7 +57,7 @@ void listen(){
 			uart.flush_buffer();
 		
 		}
-		if(strncmp(uart.receive_buffer,FETCH_ADC3,(sizeof(FETCH_ADC3)/sizeof(char))-1) == 0){
+		else if(strncmp(uart.receive_buffer,FETCH_ADC3,(sizeof(FETCH_ADC3)/sizeof(char))-1) == 0){
 			adc.convert();
 			_delay_us(500);
 			itoa(adc_values.channel_3,response,10);
@@ -66,7 +66,7 @@ void listen(){
 			uart.flush_buffer();
 		
 		}
-		if(strncmp(uart.receive_buffer,FETCH_ADC4,(sizeof(FETCH_ADC4)/sizeof(char))-1) == 0){
+		else if(strncmp(uart.receive_buffer,FETCH_ADC4,(sizeof(FETCH_ADC4)/sizeof(char))-1) == 0){
 			adc.convert();
 			_delay_us(500);
 			itoa(adc_values.channel_4,response,10);
@@ -75,7 +75,7 @@ void listen(){
 			uart.flush_buffer();
 		
 		}
-		if(strncmp(uart.receive_buffer,FETCH_ADC5,(sizeof(FETCH_ADC5)/sizeof(char))-1) == 0){
+		else if(strncmp(uart.receive_buffer,FETCH_ADC5,(sizeof(FETCH_ADC5)/sizeof(char))-1) == 0){
 			adc.convert();
 			_delay_us(500);
 			itoa(adc_values.channel_5,response,10);
@@ -84,7 +84,7 @@ void listen(){
 			uart.flush_buffer();
 		
 		}
-		if(strncmp(uart.receive_buffer,FETCH_ADC6,(sizeof(FETCH_ADC6)/sizeof(char))-1) == 0){
+		else if(strncmp(uart.receive_buffer,FETCH_ADC6,(sizeof(FETCH_ADC6)/sizeof(char))-1) == 0){
 			adc.convert();
 			_delay_us(500);
 			itoa(adc_values.channel_6,response,10);
@@ -93,10 +93,82 @@ void listen(){
 			uart.flush_buffer();
 		
 		}
-		if(strncmp(uart.receive_buffer,FETCH_ADC7,(sizeof(FETCH_ADC7)/sizeof(char))-1) == 0){
+		else if(strncmp(uart.receive_buffer,FETCH_ADC7,(sizeof(FETCH_ADC7)/sizeof(char))-1) == 0){
 			adc.convert();
 			_delay_us(500);
 			itoa(adc_values.channel_7,response,10);
+			uart.send_string(response);
+			uart.send_char(new_line);
+			uart.flush_buffer();
+		
+		}
+		else if(strncmp(uart.receive_buffer,FETCH_ADC8,(sizeof(FETCH_ADC8)/sizeof(char))-1) == 0){
+			adc.convert();
+			_delay_us(500);
+			itoa(adc_values.channel_8,response,10);
+			uart.send_string(response);
+			uart.send_char(new_line);
+			uart.flush_buffer();
+		
+		}
+		else if(strncmp(uart.receive_buffer,FETCH_ADC9,(sizeof(FETCH_ADC9)/sizeof(char))-1) == 0){
+			adc.convert();
+			_delay_us(500);
+			itoa(adc_values.channel_9,response,10);
+			uart.send_string(response);
+			uart.send_char(new_line);
+			uart.flush_buffer();
+		
+		}
+		else if(strncmp(uart.receive_buffer,FETCH_ADC10,(sizeof(FETCH_ADC10)/sizeof(char))-1) == 0){
+			adc.convert();
+			_delay_us(500);
+			itoa(adc_values.channel_10,response,10);
+			uart.send_string(response);
+			uart.send_char(new_line);
+			uart.flush_buffer();
+		
+		}
+		else if(strncmp(uart.receive_buffer,FETCH_ADC11,(sizeof(FETCH_ADC11)/sizeof(char))-1) == 0){
+			adc.convert();
+			_delay_us(500);
+			itoa(adc_values.channel_11,response,10);
+			uart.send_string(response);
+			uart.send_char(new_line);
+			uart.flush_buffer();
+		
+		}
+		else if(strncmp(uart.receive_buffer,FETCH_ADC12,(sizeof(FETCH_ADC12)/sizeof(char))-1) == 0){
+			adc.convert();
+			_delay_us(500);
+			itoa(adc_values.channel_12,response,10);
+			uart.send_string(response);
+			uart.send_char(new_line);
+			uart.flush_buffer();
+		
+		}
+		else if(strncmp(uart.receive_buffer,FETCH_ADC13,(sizeof(FETCH_ADC13)/sizeof(char))-1) == 0){
+			adc.convert();
+			_delay_us(500);
+			itoa(adc_values.channel_13,response,10);
+			uart.send_string(response);
+			uart.send_char(new_line);
+			uart.flush_buffer();
+		
+		}
+		else if(strncmp(uart.receive_buffer,FETCH_ADC14,(sizeof(FETCH_ADC14)/sizeof(char))-1) == 0){
+			adc.convert();
+			_delay_us(500);
+			itoa(adc_values.channel_14,response,10);
+			uart.send_string(response);
+			uart.send_char(new_line);
+			uart.flush_buffer();
+		
+		}
+		else if(strncmp(uart.receive_buffer,FETCH_ADC15,(sizeof(FETCH_ADC15)/sizeof(char))-1) == 0){
+			adc.convert();
+			_delay_us(500);
+			itoa(adc_values.channel_15,response,10);
 			uart.send_string(response);
 			uart.send_char(new_line);
 			uart.flush_buffer();
